9eb5c94219af542b4319556a8379d7518afab11f
gracekabaghe <gracema2002@gmail.com>
2014-03-03 22:18:47 +0800

    working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'faker'
 
+User.destroy_all
+Post.destroy_all
+Topic.destroy_all
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -25,14 +29,65 @@ rand(4..10).times do
     rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'youremail.com', password: 'helloworld', password_confirmation: 'helloworld')
+  u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+
+u = User.new(
+  name: 'Grace',
+  email: 'gracema2002@gmail.com',
+  password: 'password',
+  password_confirmation: 'password'
+  )
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"    
+puts "#{Comment.count} comments created"   
+  topics = []
+    15.times do
+    topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+      )
+    end 
+    rand(5..12).times do
+    topic = topics.first # getting the first topic here
+    p = u.posts.create(
+      topic: topic,
+      title: Faker::Lorem.words(rand(1..10)).join(" "), 
+      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+    # set the created_at to a time within the past year
+    p.update_attribute(:created_at, Time.now - rand(600..31536000))
+
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
+
+    # comments...
+    end
     end
   end
 end
