fc4d13a9d18c90c7d27f33c8b3051d748e3f45ab
gracekabaghe <gracema2002@gmail.com>
2014-03-04 00:26:39 +0800

    comming changes to seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,14 @@ User.destroy_all
 Post.destroy_all
 Topic.destroy_all
 
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end 
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -20,12 +28,16 @@ rand(4..10).times do
   # to avoid sending an email. The `save` method updates the database.
 
   rand(5..12).times do
+    topic = topics.first #getting the first topic here
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
-
+    topic.rotate!
+    # commets ....
+  end  
     rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
@@ -64,27 +76,6 @@ u = User.new(
 u.skip_confirmation!
 u.save
 
-
-  topics = []
-    15.times do
-    topics << Topic.create(
-    name: Faker::Lorem.words(rand(1..10)).join(" "), 
-    description: Faker::Lorem.paragraph(rand(1..4))
-      )
-    end 
-    rand(5..12).times do
-    topic = topics.first # getting the first topic here
-    p = u.posts.create(
-      topic: topic,
-      title: Faker::Lorem.words(rand(1..10)).join(" "), 
-      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
-    # set the created_at to a time within the past year
-    p.update_attribute(:created_at, Time.now - rand(600..31536000))
-
-    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
-
-    # comments...
-    end
     end
   end
 end
