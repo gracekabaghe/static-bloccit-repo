f65980c8dc112e389688dfa150f094201af274b5
Charlie Gaines <beaugaines@gmail.com>
2014-03-05 08:24:49 -0500

    seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 require 'faker'
 
+User.destroy_all
+Post.destroy_all
+Topic.destroy_all
 
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end 
 
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
@@ -18,20 +28,57 @@ rand(4..10).times do
   # to avoid sending an email. The `save` method updates the database.
 
   rand(5..12).times do
+    topic = topics.first #getting the first topic here
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
-
+    topic.rotate!
+    # commets ....
+  end  
     rand(3..7).times do
       p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n")
-      )
+        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+
+u = User.new(
+  name: 'Grace',
+  email: 'gracema2002@gmail.com',
+  password: 'password',
+  password_confirmation: 'password'
+  )
+u.skip_confirmation!
+u.save
+
     end
   end
 end
-
 u = User.first
 u.skip_reconfirmation!
 u.update_attributes(email: 'gracema2002@gmail.com', password: 'password', password_confirmation: 'password')
@@ -39,3 +86,4 @@ u.update_attributes(email: 'gracema2002@gmail.com', password: 'password', passwo
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"   
